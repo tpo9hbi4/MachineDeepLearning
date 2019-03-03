59d74b34bb5a78951c87bb29e14185a480899720
tpo9hbi4 <max.dunaev@mail.ru>
2019-03-03 23:56:26 +0300

    Create linear_model.py

---
+++ ClassificationAndRegression(4)/linear_model.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Загрузим датасет
+diabetes = datasets.load_diabetes()
+print(diabetes)
+
+# Возьмём один признак
+diabetes_X = diabetes.data[:, np.newaxis, 2]
+print(diabetes_X)
+
+# Выделим обучающие и тестовые признаки
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+# Выделим обучающие и тестовые ответы
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+# Создаём объект линейной регрессии
+regr = linear_model.LinearRegression()
+
+# обучим модель
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# Предскажем
+diabetes_y_pred = regr.predict(diabetes_X_test)
+
+# Коэффециенты регрессии
+print('Coefficients: \n', regr.coef_)
+# Метрики оценки
+print("Mean squared error: %.2f"
+      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+
+print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
+
+# Нарисуем это всё
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
